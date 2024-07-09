51b25e33a59cd64d4ba8f65e78b95a528aa0620c
Mafiuz04 <mathew.modrzynski@gmailc.om>
2024-07-09 22:42:26 +0200

    project created

---
+++ src/main/java/com/Mafiuz04/medical_proxy/MedicalProxyApplication.java
@@ -0,0 +1,13 @@
+package com.Mafiuz04.medical_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Mafiuz04/medical_proxy/MedicalProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mafiuz04.medical_proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
